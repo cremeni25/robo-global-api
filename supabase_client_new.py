a1df9dd17c86b5902ca363a13c7ff6772660164e
cremeni25 <andersonnavarro.si@gmail.com>
2025-12-05 00:54:36 -0300

    Create supabase_client_new.py

---
+++ supabase_client_new.py
@@ -0,0 +1,11 @@
+import os
+from supabase import create_client
+
+def get_supabase():
+    url = os.getenv("SUPABASE_URL")
+    key = os.getenv("SUPABASE_KEY")
+
+    if not url or not key:
+        raise Exception("Variáveis de ambiente não configuradas.")
+
+    return create_client(url, key)
